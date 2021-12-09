cc7ffbf6d7fb3ea3d93376e2165c06b947c336a2
Matthew M. Burke <matthew@bluedino.net>
2021-12-09 18:49:16 -0500

    ok, day 9, part 2 -- but this is hideous

--- 2021/d9/main.go
+++ 2021/d9/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -64,7 +65,12 @@ func minimum(ns []int) int {
 	return minimum
 }
 
-func part1(heights [][]int) {
+type Point struct {
+	Row int
+	Col int
+}
+
+func part1(heights [][]int) (lps []Point) {
 	risk := 0
 
 	for i, row := range heights {
@@ -72,21 +78,102 @@ func part1(heights [][]int) {
 			neighbors := getNeighbors(i, j, heights)
 			m := minimum(neighbors)
 			if h < m {
+				lps = append(lps, Point{Row: i, Col: j})
 				risk += (1 + h)
 			}
 		}
 	}
 
 	fmt.Println(risk)
+
+	return
 }
 
-func part2(heights [][]int) {
+func threeLargestProduct(ns []int) int {
+	sort.Ints(ns)
+	l := len(ns)
+
+	return (ns[l-1] * ns[l-2] * ns[l-3])
+}
+
+func getBasinSize(heights [][]int, lp Point) int {
+	visited := make(map[Point]bool)
+	queue := make([]Point, 0)
+
+	height := len(heights)
+	width := len(heights[0])
+
+	queue = append(queue, lp)
+	for len(queue) > 0 {
+		c := queue[0]
+		queue = queue[1:]
+
+		// Could simplify this a lot if we used points in part 1 and
+		// rewrote getNeighbors accordingly
+
+		// for each neighbor, if not seen, and height < 9, add to queue
+
+		i := c.Row
+		j := c.Col
+
+		if i != 0 {
+			x, y := i-1, j
+			_, seen := visited[Point{Row: x, Col: y}]
+			if !seen && heights[i-1][j] < 9 {
+				queue = append(queue, Point{Row: i - 1, Col: j})
+			}
+		}
+
+		if i != (height - 1) {
+			x, y := i+1, j
+			_, seen := visited[Point{Row: x, Col: y}]
+			if !seen && heights[i+1][j] < 9 {
+				queue = append(queue, Point{Row: i + 1, Col: j})
+			}
+		}
+
+		if j != 0 {
+			x, y := i, j-1
+			_, seen := visited[Point{Row: x, Col: y}]
+			if !seen && heights[i][j-1] < 9 {
+				queue = append(queue, Point{Row: i, Col: j - 1})
+			}
+		}
+
+		if j != (width - 1) {
+			x, y := i, j+1
+			_, seen := visited[Point{Row: x, Col: y}]
+			if !seen && heights[i][j+1] < 9 {
+				queue = append(queue, Point{Row: i, Col: j + 1})
+			}
+		}
+
+		// Finally, mark the current point as in the basin
+		visited[c] = true
+	}
+
+	return len(visited)
+}
+
+func getBasinSizes(heights [][]int, lps []Point) (sizes []int) {
+	for _, lp := range lps {
+		fmt.Println("calculating basin for", lp)
+		size := getBasinSize(heights, lp)
+		sizes = append(sizes, size)
+	}
+	return
+}
+
+func part2(heights [][]int, lps []Point) {
+	basinSizes := getBasinSizes(heights, lps)
+	basinProduct := threeLargestProduct(basinSizes)
+	fmt.Println(basinProduct)
 }
 
 func main() {
 	heights := readHeights()
-	part1(heights)
-	part2(heights)
+	lps := part1(heights)
+	part2(heights, lps)
 }
 
 // Local Variables:
