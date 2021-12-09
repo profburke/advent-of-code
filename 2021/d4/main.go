309bb6eb0aa0b625c6d227fadadf8415a5efdcbd
Matthew M. Burke <matthew@bluedino.net>
2021-12-09 18:50:42 -0500

    didn't commit day 4, part 2

--- 2021/d4/main.go
+++ 2021/d4/main.go
@@ -8,13 +8,23 @@ import (
 	"strings"
 )
 
+func copySlice(s []Board) (r []Board) {
+	r = make([]Board, len(s))
+	copy(r, s)
+
+	return
+}
+
 type Entry struct {
 	Number int
 	Called bool
 }
 
 type Row []Entry
-type Board []Row
+type Board struct {
+	Rows  []Row
+	Bingo bool
+}
 
 func readData() (numbers []int, boards []Board) {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -30,12 +40,15 @@ func readData() (numbers []int, boards []Board) {
 	scanner.Scan() // skip blank line
 
 	board := Board{}
+	rows := make([]Row, 0)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
+			board.Rows = rows
 			boards = append(boards, board)
 			board = Board{}
+			rows = make([]Row, 0)
 			continue
 		}
 
@@ -46,8 +59,9 @@ func readData() (numbers []int, boards []Board) {
 			e := Entry{Number: n}
 			row = append(row, e)
 		}
-		board = append(board, row)
+		rows = append(rows, row)
 	}
+	board.Rows = rows
 	boards = append(boards, board)
 
 	return
@@ -55,7 +69,7 @@ func readData() (numbers []int, boards []Board) {
 
 func mark(n int, boards *[]Board) {
 	for _, board := range *boards {
-		for _, row := range board {
+		for _, row := range board.Rows {
 			for i, entry := range row {
 				if n == entry.Number {
 					row[i] = Entry{Number: n, Called: true}
@@ -66,7 +80,7 @@ func mark(n int, boards *[]Board) {
 }
 
 func scoreBoard(board Board) (total int) {
-	for _, row := range board {
+	for _, row := range board.Rows {
 		for _, e := range row {
 			if !e.Called {
 				total += e.Number
@@ -88,7 +102,7 @@ func checkRow(row Row) bool {
 }
 
 func checkColumn(board Board, i int) bool {
-	for _, row := range board {
+	for _, row := range board.Rows {
 		if !row[i].Called {
 			return false
 		}
@@ -97,22 +111,33 @@ func checkColumn(board Board, i int) bool {
 	return true
 }
 
+func boardWins(board Board) (total int, won bool) {
+	for i, row := range board.Rows {
+		if checkRow(row) {
+			won = true
+			total = scoreBoard(board)
+		}
+		if checkColumn(board, i) {
+			won = true
+			total = scoreBoard(board)
+		}
+	}
+
+	return
+}
+
 func winner(boards []Board) (total, boardNum int, won bool) {
 	for b, board := range boards {
-		for i, row := range board {
-			if checkRow(row) {
-				won = true
-				total = scoreBoard(board)
-				boardNum = b
-				return
-			}
+		// this is pointless for part 1
+		// and not used in part 2, so ... ???
+		if board.Bingo {
+			continue
+		}
 
-			if checkColumn(board, i) {
-				won = true
-				total = scoreBoard(board)
-				boardNum = b
-				return
-			}
+		total, won = boardWins(board)
+		if won {
+			boardNum = b
+			return
 		}
 	}
 
@@ -135,12 +160,40 @@ func part1(numbers []int, boards []Board) {
 	fmt.Println(score)
 }
 
+type BoardRecord struct {
+	B     Board
+	Score int
+}
+
 func part2(numbers []int, boards []Board) {
+	boardsInPlay := copySlice(boards)
+
+	finishedBoards := make([]BoardRecord, 0)
+
+	for _, n := range numbers {
+		mark(n, &boardsInPlay)
+
+		continueBoards := make([]Board, 0)
+		for _, board := range boardsInPlay {
+
+			total, won := boardWins(board)
+			if won {
+				finishedBoards = append(finishedBoards,
+					BoardRecord{B: board, Score: n * total})
+			} else {
+				continueBoards = append(continueBoards, board)
+			}
+		}
+
+		boardsInPlay = continueBoards
+	}
+
+	fmt.Println(finishedBoards[len(finishedBoards)-1].Score)
 }
 
 func main() {
 	numbers, boards := readData()
-	part1(numbers, boards)
+	part1(numbers, copySlice(boards))
 	part2(numbers, boards)
 }
 
