bef0943494d6438e47017214f8970f4092c7dcbf
Matthew M. Burke <matthew@bluedino.net>
2021-12-14 00:37:18 -0500

    forgot to commit d13, part 2

--- 2021/d13/main.go
+++ 2021/d13/main.go
@@ -38,7 +38,7 @@ func readDots() (dots []Point, instructions []Instruction) {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		line = strings.TrimPrefix(line, "fold along")
+		line = strings.TrimPrefix(line, "fold along ")
 		parts := strings.Split(line, "=")
 		v, _ := strconv.Atoi(parts[1])
 
@@ -54,7 +54,7 @@ func mAbs(v int) int {
 
 func foldUp(p Point, v int) Point {
 	d := mAbs(p.Y - v)
-	return Point{X: p.X, Y: v + d}
+	return Point{X: p.X, Y: v - d}
 }
 
 func foldLeft(p Point, v int) Point {
@@ -62,10 +62,9 @@ func foldLeft(p Point, v int) Point {
 	return Point{X: v - d, Y: p.Y}
 }
 
-func part1(dots []Point, instructions []Instruction) {
+func fold(dots []Point, i Instruction) (newDots []Point) {
 	translated := make([]Point, 0)
 
-	i := instructions[0]
 	for _, p := range dots {
 		var newP Point
 		if i.Axis == "y" {
@@ -86,10 +85,81 @@ func part1(dots []Point, instructions []Instruction) {
 		translated = append(translated, p)
 	}
 
-	fmt.Println(len(translated))
+	newDots = translated
+	return
+}
+
+func part1(dots []Point, instructions []Instruction) {
+	dots = fold(dots, instructions[0])
+	fmt.Println(len(dots))
+}
+
+// apparently these are all defined in go 1.17; too bad I'm using 1.16 and lazy...
+const UintSize = 64 << (^uint(0) >> 64 & 1)
+const MaxInt = 1<<(UintSize-1) - 1
+const MinInt = -MaxInt // this isn't quite right, but it's close enough
+
+func findMin(ns []int) int {
+	min := MaxInt
+
+	for _, n := range ns {
+		if n < min {
+			min = n
+		}
+	}
+
+	return min
+}
+
+func findMax(ns []int) int {
+	max := MinInt
+
+	for _, n := range ns {
+		if n > max {
+			max = n
+		}
+	}
+
+	return max
+}
+
+func display(dots []Point) {
+	xs := make([]int, 0)
+	ys := make([]int, 0)
+
+	for _, p := range dots {
+		xs = append(xs, p.X)
+		ys = append(ys, p.Y)
+	}
+
+	minX := findMin(xs)
+	minY := findMin(ys)
+	maxX := findMax(xs)
+	maxY := findMax(ys)
+
+	allPoints := make(map[Point]bool)
+	for _, p := range dots {
+		allPoints[p] = true
+	}
+
+	for y := minY; y <= maxY; y++ {
+		for x := minX; x <= maxX; x++ {
+			if _, ok := allPoints[Point{X: x, Y: y}]; ok {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func part2(dots []Point, instructions []Instruction) {
+	for _, i := range instructions {
+		dots = fold(dots, i)
+	}
+
+	display(dots)
 }
 
 func main() {
