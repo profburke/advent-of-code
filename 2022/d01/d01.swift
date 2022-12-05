52bbec1a45b088c8bc21690c44e21990399aa726
Matthew M. Burke <matthew@bluedino.net>
2022-12-05 10:35:45 -0500

    need to get 2022 check in; going to do a little clean up on day 1 code

---
+++ 2022/d01/d01.swift
@@ -0,0 +1,69 @@
+#!/usr/bin/env swift
+
+import Darwin
+
+func part1(path: String) {
+    var max = Int.min
+
+    
+    guard let file = freopen(path, "r", stdin) else {
+        print("Couldn't open file: \(path)")
+        return
+    }
+
+    defer {
+        fclose(file)
+    }
+
+    var elfTotal = 0
+    while let line = readLine() {
+        if line.isEmpty {
+            if elfTotal > max {
+                max = elfTotal
+            }
+
+            elfTotal = 0
+        } else {
+            let val = Int(line)!
+            elfTotal += val
+        }
+    }
+
+    print("Max is \(max)")
+}
+
+
+func part2(path: String) {
+    var totals: [Int] = []
+
+    
+    guard let file = freopen(path, "r", stdin) else {
+        print("Couldn't open file: \(path)")
+        return
+    }
+
+    defer {
+        fclose(file)
+    }
+
+    var elfTotal = 0
+    while let line = readLine() {
+        if line.isEmpty {
+            totals.append(elfTotal)
+            elfTotal = 0
+        } else {
+            let val = Int(line)!
+            elfTotal += val
+        }
+    }
+    totals.append(elfTotal) // don't forget the last elf
+    
+    totals.sort()
+    let index = totals.count - 3
+    let top3 = totals[index...]
+    print("Sum of top 3 is \(top3.reduce(0, +))")
+}
+
+
+// part1(path: CommandLine.arguments[1])
+part2(path: CommandLine.arguments[1])
